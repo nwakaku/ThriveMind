0ea6f1ae7246a99336125f2778c87cc364e87824
wisdom nwakaku <nwakakucc@gmail.com>
2023-12-28 09:58:58 +0100

    thrive

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,17 +1,16 @@
-"use client"
+"use client";
 
-import { Navbar } from '@/components/Navbar'
-import { Profile } from '@/components/Profile'
-import { useWeb5 } from '../web5Context';
+import { Navbar } from "@/components/Navbar";
+import { Profile } from "@/components/Profile";
+import { useWeb5 } from "../web5Context";
 
 export default function Home() {
-
-    const { myDid, createAcc } = useWeb5();
+  const { myDid, createAcc } = useWeb5();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <Navbar myDid={myDid} createAcc={createAcc} />
+      <Navbar myDid={myDid} createAcc={createAcc} />
       <Profile myDid={myDid} />
     </main>
-  )
+  );
 }
