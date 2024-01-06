d25afdc310f60139b7385adb796236843083083c
nwakakukaks <winnernwakaku123@gmail.com>
2024-01-06 04:13:55 +0100

    tickering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,12 +8,12 @@ export const Navbar = ({ myDid, createAcc }) => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
+            src="https://i.ibb.co/WGcR7Lv/2.png"
+            className="h-12 w-12 bg-gray-100 p-1 rounded-full font-extrabold text-blue-200"
+            alt="trive Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            ThriveMind
+            TriveMind
           </span>
         </a>
 
